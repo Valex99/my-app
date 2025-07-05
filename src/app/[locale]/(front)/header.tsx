f97699c6456d1e8722cbe28f7d058351db520075
valex <mval.fx@gmail.com>
2025-07-05 21:50:21 +0200

    Create header

--- src/app/[locale]/(front)/header.tsx
+++ src/app/[locale]/(front)/header.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import { Menu } from "lucide-react";
 
 export default function Header() {
   return (
     <header className="bg-white shadow-sm">
-      <nav className="container mx-auto px-4 py-4">
+      <nav className=" mx-auto px-4 py-4">
         <div className="flex items-center justify-between text-rolex-green">
           <Link href="/" className="text-xl font-bold">
             Watch Project
           </Link>
-          <div className="hidden md:flex space-x-6">
-            <Link href="/about-us" className="hover:text-gray-600">
-              About Us
-            </Link>
-            <Link href="/gallery" className="hover:text-gray-600">
-              Gallery
-            </Link>
-            <Link href="/reservations" className="hover:text-gray-600">
-              Reservations
-            </Link>
-          </div>
+          <Menu className="w-6 h-6" />
         </div>
       </nav>
     </header>
